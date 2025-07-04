7753424f4ebf9508a8cc5e305b8435f8f9eb1b28
Sonali Sahu <sonalisahu0064@gmail.com>
2025-07-04 21:33:30 +0530

    Completed hashing theory at day 13

---
+++ 13_DAY_HASHING_PART_A/HashingBasics.java
@@ -0,0 +1,31 @@
+public class HashingBasics {
+
+    // Create a frequency array to store occurrences
+    // Adjust size based on max possible element in arr
+    static int[] hash = new int[100];
+
+    // Function to calculate frequency of all elements
+    public static void buildHash(int[] arr) {
+        for (int num : arr) {
+            hash[num]++;
+        }
+    }
+
+    // Function to return the count of a queried number
+    public static int fn(int q, int[] arr) {
+        return hash[q];
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 1, 1, 2, 3, 4, 5, 6, 6, 6, 7, 5 };
+
+        // Build the hash array from input array
+        buildHash(arr);
+
+        System.out.println("Occurrences:");
+        int querys[] = { 1, 3, 4, 2, 5, 12 };
+        for (int q : querys) {
+            System.out.println(q + " = " + fn(q, arr));
+        }
+    }
+}
